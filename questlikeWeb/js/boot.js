2a94a12b836c0444ace9948edcec9e944fd92465
ghenry <ghenry@spritewrench.com>
2022-05-28 12:50:47 -0500

    update

--- questlikeWeb/js/boot.js
+++ questlikeWeb/js/boot.js
@@ -26,15 +26,20 @@
        
       if(navigator.userAgentData.mobile === true){
         this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-        this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
       }      
       else{
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.scale.pageAlignHorizontally = true;
-        this.game.scale.pageAlignVertically = true;          
+        this.game.scale.pageAlignVertically = true;     
+        if(screen.width < 1024){
+          //alert(screen.width)  
+          this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;   
+          this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+  
+        } 
       }
-
-
+      
+      //this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
       this.game.renderer.renderSession.roundPixels = true;
       
 
