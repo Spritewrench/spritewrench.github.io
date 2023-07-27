25ee9361358e703f92863deb832152dd0a2186db
ghenry <ghenry@spritewrench.com>
2023-07-27 16:30:26 -0500

    Salt update

---
+++ sunkenStones-Bleeding/js/menu.js
@@ -0,0 +1,101 @@
+(function() {
+  'use strict';
+
+  function Menu() {
+    this.titleTxt = null;
+    this.startTxt = null;
+  }
+
+  Menu.prototype = {
+
+    create: function () {
+      var x = this.game.width / 2
+        , y = this.game.height / 2;
+
+      this.bg = this.add.sprite(0, 0, 'bg');
+      this.bg.width = this.game.width
+      this.bg.height = this.game.height      
+      //this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'T.C.P',16);
+
+      this.overlay = this.add.sprite(0, -300, 'bgOverlay');
+      this.overlay.width = this.game.width
+      this.overlay.height = this.game.height+300  
+      this.overlay.alpha = 0.6   
+
+      this.titleTxt = this.add.text(x,y, 'Sunken Stones', {font: '64px LondrinaSolid-Black',fill: '#fff', align: 'center'});
+      this.titleTxt.anchor.setTo(0.5, 0.5);
+
+      this.titleTxt2 = this.add.text(x-125,this.titleTxt.y+100, 'High Score:', {font: '50px LondrinaSolid-Black',fill: '#fff', align: 'center'});
+      this.titleTxt2.anchor.setTo(0.5, 0.5);      
+
+      this.scoreCountUI = this.add.sprite(x+125,this.titleTxt.y+100, 'coinCount');
+      this.scoreCountUI.anchor.setTo(0.5, 0.5);  
+
+      if(sessionStorage.getItem("highScore") == null){
+    
+        sessionStorage.setItem("highScore", 0 ) 
+      }
+
+      this.scoreCount = this.add.text(this.scoreCountUI.x,this.scoreCountUI.y, sessionStorage.getItem("highScore"), {font: '48px LondrinaSolid-Black',fill: '#fff', align: 'left'});
+      this.scoreCount.anchor.setTo(0.5, 0.5);        
+      
+      this.titleTxt3 = this.add.text(x,this.game.height-100, '[CLICK TO START]', {font: '32px LondrinaSolid-Black',fill: '#fff', align: 'center'});
+      this.titleTxt3.anchor.setTo(0.5, 0.5);     
+
+      y = y + this.titleTxt.height + 5;
+      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', '',16);
+      this.startTxt.anchor.setTo(0.5, 0.5);
+
+      var T = "";
+      var C = "";
+      var P = ""
+      
+      var tnum = Math.floor(Math.random() * 6);
+      var cnum = Math.floor(Math.random() * 6);
+      var pnum = Math.floor(Math.random() * 6);
+      
+      
+      this.count =2;
+      this.sprite = this.add.sprite(0, 0, 'wrench');
+      this.sprite.width = this.game.width
+      this.sprite.height = this.game.height
+
+      
+      
+
+      this.sprite.inputEnabled = true;
+      this.sprite.events.onInputDown.add(this.onClick, this);
+
+      this.input.onDown.add(this.onDown, this);
+      
+    },
+
+    update: function () {
+
+    },
+    onClick: function (pic) {
+      pic.x = -this.game.width;
+      this.count++;
+    },
+    onDown: function () {
+      localStorage.setItem("level","1")
+      if(this.count >= 3){
+        this.game.state.start('game');
+      }
+	    //
+        /*var user = firebase.auth().currentUser;
+
+        if (user) {
+        // User is signed in.
+            this.game.state.start('game');
+        } else {
+        // No user is signed in.
+        } */       
+        
+    }
+  };
+
+  window['simplewar'] = window['simplewar'] || {};
+  window['simplewar'].Menu = Menu;
+
+}());
---
+++ sunkenStones-Bleeding/js/preloader.js
@@ -0,0 +1,151 @@
+(function () {
+    'use strict';
+
+    function Preloader() {
+        this.asset = null;
+        this.ready = false;
+    }
+    Preloader.prototype = {
+        preload: function () {
+            this.asset = this.add.sprite(320, 240, 'preloader');
+            this.asset.anchor.setTo(0.5, 0.5);
+            this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+            this.load.setPreloadSprite(this.asset);
+            //this.load.audio('bg', ['sound/LeavingYouBehind.ogg']);
+            
+            this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
+          
+            this.load.image('tile', 'assets/tile.png');
+            this.load.image('hero', 'assets/hero.png');
+            this.load.image('mon', 'assets/mon.png');
+
+            this.load.image('mon-0', 'assets/tile.png')
+            this.load.image('mon-1', 'assets/mon-0.png');
+            this.load.image('mon-2', 'assets/mon-1.png');
+            this.load.image('mon-3', 'assets/mon-2.png');
+            this.load.image('mon-4', 'assets/mon-3.png');
+            this.load.image('mon-5', 'assets/mon-4.png');
+            this.load.image('mon-6', 'assets/mon-5.png');
+            this.load.image('mon-7', 'assets/mon-6.png');
+            this.load.image('mon-8', 'assets/mon-7.png');  
+            this.load.image('mon-9', 'assets/mon-8.png');  
+            this.load.image('mon-10', 'assets/mon-9.png');  
+            this.load.image('mon-99', 'assets/mon-99.png'); 
+            
+            this.load.image('treasure_1', 'assets/treasure_1.png');
+            this.load.image('treasure_2', 'assets/treasure_2.png');
+            this.load.image('treasure_3', 'assets/treasure_3.png');
+            this.load.image('treasure_4', 'assets/treasure_4.png');
+            this.load.image('treasure_5', 'assets/treasure_5.png');
+            this.load.image('treasure_6', 'assets/treasure_6.png');
+            this.load.image('treasure_7', 'assets/treasure_7.png');
+            this.load.image('treasure_8', 'assets/treasure_8.png');
+            this.load.image('treasure_9', 'assets/treasure_9.png');
+
+            this.load.image('treasureUI_1', 'assets/treasureUI_1.png');
+            this.load.image('treasureUI_2', 'assets/treasureUI_2.png');
+            this.load.image('treasureUI_3', 'assets/treasureUI_3.png');
+            this.load.image('treasureUI_4', 'assets/treasureUI_4.png');
+            this.load.image('treasureUI_5', 'assets/treasureUI_5.png');
+            this.load.image('treasureUI_6', 'assets/treasureUI_6.png');
+            this.load.image('treasureUI_7', 'assets/treasureUI_7.png');
+            this.load.image('treasureUI_8', 'assets/treasureUI_8.png');
+            this.load.image('treasureUI_9', 'assets/treasureUI_9.png');            
+
+          
+
+            this.load.image('crew-1', 'assets/crew-0.png');
+            this.load.image('crew-2', 'assets/crew-1.png');
+            this.load.image('crew-3', 'assets/crew-2.png');
+            this.load.image('crew-4', 'assets/crew-3.png');
+            this.load.image('crew-5', 'assets/crew-4.png');
+
+            this.load.image('blankCrew', 'assets/blankCrew.png');
+
+            this.load.image('buff_1', 'assets/buff_1.png');
+            this.load.image('buff_2', 'assets/buff_2.png');
+
+            this.load.image('freeCounter', 'assets/freeCounter.png');
+
+            this.load.image('crewSelect', 'assets/crewSelect.png');
+            this.load.image('crewDeployed', 'assets/crewDeployed.png');
+
+            this.load.image('stamina', 'assets/stamina.png');
+            this.load.image('heart', 'assets/heart.png');
+
+            this.load.image('turnMarker', 'assets/turnMarker.png');
+          
+            this.load.image('bg', 'assets/bg.png');
+            this.load.image('winBG', 'assets/winBG.png');
+            this.load.image('loseBG', 'assets/loseBG.png');
+            this.load.image('bgOverlay', 'assets/bgOverlay2.png');
+            this.load.image('ui_back', 'assets/ui_back.png');
+            this.load.image('combatOrder', 'assets/combatOrder.png');
+
+            this.load.image('coinCount', 'assets/coinCount.png');
+
+            this.load.image('selectInfoPanel', 'assets/selectInfoPanel.png');
+            this.load.image('capInfoPanel', 'assets/capInfoPanel.png');
+
+            this.load.image('selectInfoPanel-0', 'assets/selectInfoPanel-0.png');
+            this.load.image('selectInfoPanel-1', 'assets/selectInfoPanel-1.png');
+            this.load.image('selectInfoPanel-2', 'assets/selectInfoPanel-2.png');
+            this.load.image('selectInfoPanel-3', 'assets/selectInfoPanel-3.png');
+            this.load.image('selectInfoPanel-4', 'assets/selectInfoPanel-4.png');
+
+            this.load.image('selectInfoPanel-101', 'assets/selectInfoPanel-101.png');
+            this.load.image('selectInfoPanel-102', 'assets/selectInfoPanel-102.png');
+            this.load.image('selectInfoPanel-103', 'assets/selectInfoPanel-103.png');
+            this.load.image('selectInfoPanel-104', 'assets/selectInfoPanel-104.png');    
+            this.load.image('selectInfoPanel-105', 'assets/selectInfoPanel-105.png');
+            this.load.image('selectInfoPanel-106', 'assets/selectInfoPanel-106.png');
+            this.load.image('selectInfoPanel-107', 'assets/selectInfoPanel-107.png');
+            this.load.image('selectInfoPanel-108', 'assets/selectInfoPanel-108.png'); 
+            this.load.image('selectInfoPanel-109', 'assets/selectInfoPanel-109.png');  
+            this.load.image('selectInfoPanel-199', 'assets/selectInfoPanel-199.png');    
+            
+
+            
+            this.load.image('ration', 'assets/fruit_t.png');    
+
+            this.load.image('saltUI', 'assets/saltUI.png');    
+            this.load.image('chestUI', 'assets/chestUI.png');    
+            this.load.image('saltMeter', 'assets/saltMeter.png'); 
+            this.load.image('saltMeterBack', 'assets/saltMeterBack.png');    
+
+            this.load.image('ui_cap_health', 'assets/cap_health.png');
+            this.load.image('ui_deploy_pool', 'assets/deploy_pool.png');
+
+            this.load.image('ui_endTurn_Button', 'assets/endTurn_button.png');
+            this.load.image('ui_clear_button', 'assets/clear_button.png');
+
+            this.load.image('ui_ult_buttonReady', 'assets/ult_buttonReady.png');
+            this.load.image('ui_ult_buttonNotReady', 'assets/ult_buttonNotReady.png');
+
+            this.load.image('listenmi', 'assets/listenMi.png');
+            this.load.image('wrench', 'assets/wrench.png');
+            this.load.image('ggj', 'assets/ggj.png');
+
+            this.load.image('overlay', 'assets/overlay.png');
+            this.load.image('heroHuge', 'assets/heroHuge.png');
+            this.load.image('monHuge', 'assets/monHuge.png');
+            this.load.image('textBox', 'assets/textBox.png');
+            this.load.image('textBoxFoe', 'assets/textBoxFoe.png');
+            
+          
+        }
+        , create: function () {
+            this.asset.cropEnabled = false;
+        }
+        , update: function () {
+            if (!!this.ready) {
+                this.game.state.start('menu');
+            }
+        }
+        , onLoadComplete: function () {
+            this.ready = true;
+        }
+    };
+    window['simplewar'] = window['simplewar'] || {};
+    window['simplewar'].Preloader = Preloader;
+}());
